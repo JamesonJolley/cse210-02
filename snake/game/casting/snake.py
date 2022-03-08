2f11c5dd2e3fdc2f495fd6632a61fffa8786c5b7
JamesonJolley <jol21004@byui.edu>
2022-03-08 06:44:46 -0700

    added a trall on the snakes

--- snake/game/casting/snake.py
+++ snake/game/casting/snake.py
@@ -20,33 +20,65 @@ class Snake(Actor):
     def get_segments(self):
         return self._segments
 
+    # old will depracate
+    #def move_next(self):
+    #    # move all segments
+    #    for segment in self._segments:
+    #        segment.move_next()
+    #    # update velocities
+    #    for i in range(len(self._segments) - 1, 0, -1):
+    #        trailing = self._segments[i]
+    #        previous = self._segments[i - 1]
+    #        velocity = previous.get_velocity()
+    #        trailing.set_velocity(velocity)
+
     def move_next(self):
-        # move all segments
+        #move the head
+        head = self.get_head()
+        head.move_next()
+        #add new actor
+        can_trail = True
+        # logic to avoid duplicate segments in the same position 
         for segment in self._segments:
-            segment.move_next()
-        # update velocities
-        for i in range(len(self._segments) - 1, 0, -1):
-            trailing = self._segments[i]
-            previous = self._segments[i - 1]
-            velocity = previous.get_velocity()
-            trailing.set_velocity(velocity)
+            if head.get_position() == segment.get_position():
+                if segment != head:
+                    can_trail = False
+                    break
+        if can_trail:
+            self.leave_trail()
+   
+    
+    # fution for apending segments as the tral: Jameosn
+    def leave_trail(self):
+        '''
+        creates and apends a new segment at the position of the head
+        '''
+        #makes a new segment
+        segment = Actor()
+        segment.set_position(self.get_head().get_position())
+        segment.set_text('#')
+        segment.set_color(constants.GREEN)
+        self._segments.append(segment)
+
+
 
     def get_head(self):
         return self._segments[0]
 
-    def grow_tail(self, number_of_segments):
-        for i in range(number_of_segments):
-            tail = self._segments[-1]
-            velocity = tail.get_velocity()
-            offset = velocity.reverse()
-            position = tail.get_position().add(offset)
-            
-            segment = Actor()
-            segment.set_position(position)
-            segment.set_velocity(velocity)
-            segment.set_text("#")
-            segment.set_color(constants.GREEN)
-            self._segments.append(segment)
+    # old will depracate
+#    def grow_tail(self, number_of_segments):
+#        for i in range(number_of_segments):
+#            tail = self._segments[-1]
+#            velocity = tail.get_velocity()
+#            offset = velocity.reverse()
+#            position = tail.get_position()#.add(offset)
+#            
+#            segment = Actor()
+#            segment.set_position(position)
+#            segment.set_velocity(velocity)
+#            segment.set_text("#")
+#            segment.set_color(constants.GREEN)
+#            self._segments.append(segment)
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
@@ -91,3 +123,13 @@ class snake_2 (Snake):
             segment.set_text(text)
             segment.set_color(color)
             self._segments.append(segment)
+
+    # this makes the tral, we are makeing it red with  polymorphism  : Jameson
+    def leave_trail(self):
+        #makes a new segment
+        segment = Actor()
+        #sets the position
+        segment.set_position(self.get_head().get_position())
+        segment.set_text('#')
+        segment.set_color(constants.RED)
+        self._segments.append(segment)
