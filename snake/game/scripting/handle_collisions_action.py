0fd66109d36e848b83dbfda1b68720de5e755cbe
JamesonJolley <jol21004@byui.edu>
2022-03-13 05:27:50 -0700

    updated HandleCollisionsAction to not handel segmet or food collisions

--- snake/game/scripting/handle_collisions_action.py
+++ snake/game/scripting/handle_collisions_action.py
@@ -26,8 +26,8 @@ class HandleCollisionsAction(Action):
             script (Script): The script of Actions in the game.
         """
         if not self._is_game_over:
-            self._handle_food_collision(cast)
-            self._handle_segment_collision(cast)
+            #self._handle_food_collision(cast)
+            #self._handle_segment_collision(cast)
             self._handle_game_over(cast)
 
     def _handle_food_collision(self, cast):
