4679f4ac5b1284db878dfce2ad63cf7a90d2a4e0
JamesonJolley <jol21004@byui.edu>
2022-03-17 05:23:46 -0700

    made the file

--- Final Project/__main__.py
+++ Final Project/__main__.py
@@ -1,12 +1,11 @@
 import constants
 
 from game.casting.cast import Cast
-from game.casting.score import Score , Score_2
-from game.casting.snake import Snake , snake_2
+from game.casting.score import Score 
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction,ControlActorsAction_2
 from game.scripting.move_actors_action import MoveActorsAction
-from game.scripting.handle_collisions_action import HandleCollisionsAction
+from game.casting.actor import Actor
 from game.scripting.draw_actors_action import DrawActorsAction
 from game.directing.director import Director
 from game.services.keyboard_service import KeyboardService
@@ -15,17 +14,14 @@ from game.shared.color import Color
 from game.shared.point import Point
 
 
+
+
 def main():
     
     # create the cast
     cast = Cast()
-    #cast.add_actor("foods", Food())
-    cast.add_actor("snakes", Snake())
-    #creaing the second snake :Omarlin
-    cast.add_actor("snake2", snake_2())
     cast.add_actor("scores", Score())
-    cast.add_actor('scores2', Score_2())
-   
+    
     # start the game
     keyboard_service = KeyboardService()
     video_service = VideoService()
@@ -36,7 +32,6 @@ def main():
     script.add_action("input", ControlActorsAction_2(keyboard_service))
     
     script.add_action("update", MoveActorsAction())
-    script.add_action("update", HandleCollisionsAction())
     script.add_action("output", DrawActorsAction(video_service))
     
     director = Director(video_service)
---
+++ Final Project/game/scripting/draw_actors_action.py
@@ -0,0 +1,64 @@
+from game.scripting.action import Action
+
+
+class DrawActorsAction(Action):
+    """
+    An output action that draws all the actors.
+    
+    The responsibility of DrawActorsAction is to draw all the actors.
+
+    Attributes:
+        _video_service (VideoService): An instance of VideoService.
+    """
+
+    def __init__(self, video_service):
+        """Constructs a new DrawActorsAction using the specified VideoService.
+        
+        Args:
+            video_service (VideoService): An instance of VideoService.
+        """
+        self._video_service = video_service
+
+    def execute(self, cast, script):
+        """Executes the draw actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        #score = cast.get_first_actor("scores")
+        #score2 = cast.get_first_actor("scores2")
+        ##food = cast.get_first_actor("foods")
+        #snake = cast.get_first_actor("snakes")
+        #segments = snake.get_segments()
+#
+        ##this is to get all the pieces for the red snake :Omarlin
+        #snake_2 = cast.get_first_actor("snake2")
+        #segments_2 = snake_2.get_segments()
+        #
+        #messages = cast.get_actors("messages")
+#
+        #
+        ##self._video_service.draw_actor(food)
+        #self._video_service.draw_actors(segments)
+        ##this is to draw the second snake :Omarlin
+        #self._video_service.draw_actors(segments_2)
+        ##this is to draw the second score board :Jameson
+        #self._video_service.draw_actor(score2)
+#
+        #self._video_service.draw_actor(score)
+        #self._video_service.draw_actors(messages, True)
+        
+
+        actors = cast.get_all_actors()
+        self._video_service.clear_buffer()
+
+        for _actor in actors:
+            try:
+                self._video_service.draw_actors(_actor)
+            except TypeError as TE:
+                self._video_service.draw_actor(_actor)
+            else:
+                print("somthing whent rong")
+
+        self._video_service.flush_buffer()    
